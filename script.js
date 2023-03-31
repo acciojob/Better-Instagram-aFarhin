bb7b14dc14f19fdaae446a1ad113b07cab6cf2f2
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-31 16:34:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+const parent = document.querySelector("#parent");
+let selectedDiv = null;
+
+parent.addEventListener("dragstart", (e) => {
+  selectedDiv = e.target;
+});
+
+parent.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+parent.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const targetDiv = e.target;
+  const parent = targetDiv.parentNode;
+  if (targetDiv !== selectedDiv) {
+    parent.insertBefore(selectedDiv, targetDiv);
+  }
+});
